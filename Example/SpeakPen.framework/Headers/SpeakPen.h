2cc9db84c043db10a00d5f214aa636911e3aa5fe
felix <felix@aiedevice.com>
2020-10-12 15:25:00 +0800

    0.0.5

---
+++ Example/SpeakPen.framework/Headers/SpeakPen.h
@@ -0,0 +1,22 @@
+ 
+#import <Foundation/Foundation.h>
+
+#import <SpeakPen/STPAuthApi.h>
+#import <SpeakPen/STPUserModel.h>
+#import <SpeakPen/STPDeviceModel.h>
+#import <SpeakPen/STPDeviceApi.h>
+#import <SpeakPen/STPAccessConfig.h>
+#import <SpeakPen/STPPictureBookApi.h>
+#import <SpeakPen/STPPicBookResourceModel.h>
+#import <SpeakPen/STPStudyReportApi.h>
+#import <SpeakPen/STPStudyReportModel.h>
+
+//! Project version number for SpeakPen.
+FOUNDATION_EXPORT double SpeakPenVersionNumber;
+
+//! Project version string for SpeakPen.
+FOUNDATION_EXPORT const unsigned char SpeakPenVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SpeakPen/PublicHeader.h>
+
+
--- StpOCSDK-Liu.podspec
+++ StpOCSDK-Liu.podspec
@@ -8,7 +8,7 @@
 
 Pod::Spec.new do |s|
   s.name             = 'StpOCSDK-Liu'
-  s.version          = '0.0.4'
+  s.version          = '0.0.5'
   s.summary          = 'StpOCSDK-Liu'
 
 # This description is used to generate tags and improve search results.
